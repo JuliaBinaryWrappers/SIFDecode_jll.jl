b7230ca1550a443b642489d0b8c66fc6d88f0322
jlbuild <juliabuildbot@gmail.com>
2026-05-31 09:11:32 +0000

    SIFDecode_jll build 3.1.1+0

--- src/SIFDecode_jll.jl
+++ src/SIFDecode_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("SIFDecode")
-JLLWrappers.@generate_main_file("SIFDecode", UUID("54dcf436-342f-53ea-8005-3708a1ae6c8c"))
+JLLWrappers.@generate_main_file("SIFDecode", Base.UUID("54dcf436-342f-53ea-8005-3708a1ae6c8c"))
 end  # module SIFDecode_jll
